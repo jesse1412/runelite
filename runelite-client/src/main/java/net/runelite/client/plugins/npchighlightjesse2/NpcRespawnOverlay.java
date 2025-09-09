368ede6690d984a5e086573ab21be32f4f1edfad
Jesse Serrao <jesseserrao@hotmail.co.uk>
2025-09-09 16:33:47 +0100

    Add new npc hl

--- runelite-client/src/main/java/net/runelite/client/plugins/npchighlightjesse2/NpcRespawnOverlay.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/npchighlightjesse2/NpcRespawnOverlay.java
@@ -23,7 +23,7 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-package net.runelite.client.plugins.npchighlight;
+package net.runelite.client.plugins.npchighlightjesse2;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
